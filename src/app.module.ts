3cc4461002bcad501ae6aeb8bd6200246ea86983
Anastasiia Tymchenko <tymchenko.anastasiia@gmail.com>
2024-11-07 17:05:46 +0000

    feat: artist endpoint implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,13 @@ import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
+import { ArtistController } from './artist/artist.controller';
+import { ArtistModule } from './artist/artist.module';
+import { ArtistService } from './artist/artist.service';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  imports: [DatabaseModule, UserModule, ArtistModule],
+  controllers: [AppController, UserController, ArtistController],
+  providers: [AppService, UserService, ArtistService],
 })
 export class AppModule {}
